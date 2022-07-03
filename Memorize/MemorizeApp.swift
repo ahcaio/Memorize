b3bc6b2b2ea6e115e127ef7c3e45bc1c4494ea5e
Caio Cesar <ahcaio@hotmail.com>
2022-07-03 07:53:37 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Caio Cesar Alves de Limeira on 03/07/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
