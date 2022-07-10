4dfca935bfafabeca7d432f77a258720dbdf6342
Caio Cesar <ahcaio@hotmail.com>
2022-07-10 10:19:34 -0300

    Add GeometryReader Add Drawing constants

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -19,7 +19,6 @@ struct EmojiMemoryGameView: View {
         }
             .padding()
             .foregroundColor(Color.orange)
-            .font(Font.largeTitle)
     }
 }
 
@@ -27,23 +26,34 @@ struct CardView: View {
     var card: MemoryGame<String>.Card
     
     var body: some View {
+        GeometryReader { geometry in
+            body(for: geometry.size)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View {
         ZStack {
             if card.isFaceUp {
-                RoundedRectangle(cornerRadius: 10.0)
+                RoundedRectangle(cornerRadius: cornerRadius)
                     .fill(Color.white)
-                    .frame(width: 100.0, height: 150.0)
-                RoundedRectangle(cornerRadius: 10.0)
-                    .stroke(lineWidth: 3)
-                    .frame(width: 100.0, height: 150.0)
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .stroke(lineWidth: edgeLineWidth)
                 Text(card.content)
             } else {
-                RoundedRectangle(cornerRadius: 10.0)
+                RoundedRectangle(cornerRadius: cornerRadius)
                     .fill()
-                    .frame(width: 100.0, height: 150.0)
             }
         }
-        
+        .font(Font.system(size: fontSize(for: size)))
     }
+    
+    //MARK: Drawing constants
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3
+    func fontSize (for size: CGSize) -> CGFloat {
+            min(size.width, size.height) * 0.75
+    }
+    
 }
 
 
